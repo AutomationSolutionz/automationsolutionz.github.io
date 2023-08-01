f996350d1c19ed47d80fdb04bd4156d3867e1ec0
sazid <sazidozon@gmail.com>
2023-08-01 14:51:20 +0600

    Deploy website - based on 7835ee24500baf27307b1b11fd0b75ef8e1f05a5

---
+++ 8b598085.c1746600.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[56],{131:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Hello","permalink":"/blog/hello-world"}]}')}}]);
