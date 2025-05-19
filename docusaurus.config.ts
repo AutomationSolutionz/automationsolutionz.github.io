354bfcff6726f0359b9249b81ee3d407f80ebecc
Mohammed Sazid Al Rashid <sazidozon@gmail.com>
2025-05-19 17:52:34 +0600

    Update the social sharing card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,7 +69,7 @@ const config: Config = {
 
     themeConfig: {
         // Replace with your project's social card
-        image: "img/docusaurus-social-card.jpg",
+        image: "img/zeuz-social-card.png",
         navbar: {
             title: "",
             logo: {
